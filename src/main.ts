1f17281f545af28f9614f8eba4e6d44e5fd7ab88
kumane422 <127856842+kumane422@users.noreply.github.com>
2023-11-16 23:20:20 +0900

    Firebase設定とビルド

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import Vue from 'vue'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzWTJszr4ecrxwifSBU_XWPPo0Ppkm1i4",
+  authDomain: "test-something-db.firebaseapp.com",
+  projectId: "test-something-db",
+  storageBucket: "test-something-db.appspot.com",
+  messagingSenderId: "349501994541",
+  appId: "1:349501994541:web:3a330b17f54ecd0e176aa4",
+  measurementId: "G-6YRHW4W42Z"
+}
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 new Vue({
   router,
