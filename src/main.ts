44f8fa6678388689081478ef34fd68e533d932fd
kumane422 <127856842+kumane422@users.noreply.github.com>
2023-11-17 01:21:31 +0900

    Firestore設定

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import { initializeApp } from 'firebase/app'
 import VueGtag from 'vue-gtag'
 import App from './App.vue'
 import router from './router'
@@ -7,17 +6,6 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDzWTJszr4ecrxwifSBU_XWPPo0Ppkm1i4",
-  authDomain: "test-something-db.firebaseapp.com",
-  projectId: "test-something-db",
-  storageBucket: "test-something-db.appspot.com",
-  messagingSenderId: "349501994541",
-  appId: "1:349501994541:web:3a330b17f54ecd0e176aa4",
-  measurementId: "G-6YRHW4W42Z"
-}
-
-initializeApp(firebaseConfig)
 Vue.use(VueGtag, { config: { id: 'G-6YRHW4W42Z' } }, router);
 
 new Vue({
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { Animal } from '@/store/types'
 
 Vue.use(Vuex)
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzWTJszr4ecrxwifSBU_XWPPo0Ppkm1i4",
+  authDomain: "test-something-db.firebaseapp.com",
+  projectId: "test-something-db",
+  storageBucket: "test-something-db.appspot.com",
+  messagingSenderId: "349501994541",
+  appId: "1:349501994541:web:3a330b17f54ecd0e176aa4",
+  measurementId: "G-6YRHW4W42Z"
+}
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 
 // localStorageに保存されている値を取得
 const savedCount = localStorage.getItem('count')
