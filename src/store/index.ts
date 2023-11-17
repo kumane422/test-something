47d65f3ff10e724c45d54d8b9c40d371fc310d63
kumane422 <127856842+kumane422@users.noreply.github.com>
2023-11-17 17:16:49 +0900

    環境設定ファイルを用意

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,13 +7,13 @@ import { Animal } from '@/store/types'
 Vue.use(Vuex)
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzWTJszr4ecrxwifSBU_XWPPo0Ppkm1i4",
-  authDomain: "test-something-db.firebaseapp.com",
-  projectId: "test-something-db",
-  storageBucket: "test-something-db.appspot.com",
-  messagingSenderId: "349501994541",
-  appId: "1:349501994541:web:3a330b17f54ecd0e176aa4",
-  measurementId: "G-6YRHW4W42Z"
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FB_APP_ID,
+  measurementId: process.env.VUE_APP_FB_MEASUREMENT_ID
 }
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
